c7a772d2edb2b51e74156deeffc230e6c28a3763
Fernando Costa Migliorini <fercosmig@gmail.com>
2024-04-25 14:56:49 -0300

    testes

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main()
+{
+    println!("Hello, world!");
+
+    // sem declaração do tipo de dado.
+    let name_a = "Fernando";
+    println!("Hello {}!", name_a);
+
+    // declarando o tipo de dado.
+    let name_b: &str = "Fernanda";
+    println!("Hello {}!", name_b);
+}
